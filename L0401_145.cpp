f5c9a732b5ab667d94168a22d1b6c39f75476d44
Gorila101 <akbulatikx@gmail.com>
2025-10-12 22:25:09 +0500

    Update L0401_145.cpp

--- L0401_145.cpp
+++ L0401_145.cpp
@@ -1,54 +1,8 @@
 #include <iostream>
 #include <string>
-<<<<<<< HEAD:L0401_145.cpp.txt
 #include <cstdlib>
 #include <ctime>
 #include "my_qsort.h"
-=======
-#include <cstdlib>   // для rand()
-#include <ctime>     // для srand()
-
-// Универсальная функция swap
-template<typename T>
-void swap(T &a, T &b) {
-    T temp = a;
-    a = b;
-    b = temp;
-}
-
-// Функция partition с рандомным pivot
-template<typename T, typename Compare>
-int partition(T arr[], int low, int high, Compare compare) {
-    // Выбираем случайный индекс pivot между low и high
-    int randomIndex = low + rand() % (high - low + 1);
-    
-    // Меняем выбранный элемент с последним, чтобы использовать arr[high] как pivot
-    swap(arr[randomIndex], arr[high]);
-
-    T pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (compare(arr[j], pivot)) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
-}
-
-// Рекурсивная функция QuickSort
-template<typename T, typename Compare>
-void my_qsort(T arr[], int low, int high, Compare compare) {
-    if (low < high) {
-        int pi = partition(arr, low, high, compare);
-        my_qsort(arr, low, pi - 1, compare);
-        my_qsort(arr, pi + 1, high, compare);
-    }
-}
->>>>>>> a85ed9fe5a5dddd5320d903ffc06ba4264ab8dcb:L0401_145.cpp
 
 struct Student {
     std::string name;
@@ -86,8 +40,4 @@ int main() {
         std::cout << s.name << " (" << s.age << ")\n";
 
     return 0;
-<<<<<<< HEAD:L0401_145.cpp.txt
 }
-=======
-} 
->>>>>>> a85ed9fe5a5dddd5320d903ffc06ba4264ab8dcb:L0401_145.cpp
