86b4cbe28af47c188e2c632eb9068ba93c07b269
Gorila101 <akbulatikx@gmail.com>
2025-10-15 15:54:55 +0500

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include <string>
 #include <cstdlib>
 #include <ctime>
@@ -9,20 +10,21 @@ struct Student {
     int age;
 };
 
-int main() {
-    srand(static_cast<unsigned>(time(nullptr)));
+void test_int_sort() {
+    int arr[] = {42, 7, 19, 73, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int nums[] = {42, 7, 19, 73, 5};
-    int n = sizeof(nums) / sizeof(nums[0]);
-
-    my_qsort(nums, 0, n - 1, [](const int &a, const int &b) {
+    my_qsort(arr, 0, n - 1, [](const int &a, const int &b) {
         return a < b;
     });
 
-    std::cout << "Сортировка чисел: ";
-    for (int x : nums) std::cout << x << " ";
-    std::cout << "\n";
+    for (int i = 1; i < n; i++)
+        assert(arr[i - 1] <= arr[i]);
 
+    std::cout << " test_int_sort passed.\n";
+}
+
+void test_struct_sort() {
     Student students[] = {
         {"Иван", 22},
         {"Мария", 19},
@@ -35,9 +37,18 @@ int main() {
         return a.age < b.age;
     });
 
-    std::cout << "Сортировка студентов по возрасту:\n";
-    for (auto &s : students)
-        std::cout << s.name << " (" << s.age << ")\n";
+    for (int i = 1; i < m; i++)
+        assert(students[i - 1].age <= students[i].age);
 
+    std::cout << "✅ test_struct_sort passed.\n";
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    test_int_sort();
+    test_struct_sort();
+
+    std::cout << " Все тесты успешно пройдены!\n";
     return 0;
 }
